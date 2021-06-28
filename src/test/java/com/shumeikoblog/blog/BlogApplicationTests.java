0c29a7e8aa413e7d0ca959e8fb5e60e237be67d0
maks_shumeiko <shumuniver01@gmail.com>
2021-06-29 01:29:06 +0300

    29.06.2021: First commit this project. Add all file.

---
+++ src/test/java/com/shumeikoblog/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shumeikoblog.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
